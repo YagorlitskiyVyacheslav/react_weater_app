0012232559f40842fe056e0fd91f31d89916173c
Yagorlytsyi Viacheslav <47087934+YagorlitskiyVyacheslav@users.noreply.github.com>
2022-08-13 07:57:31 +0300

    Weather App init

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    danger: "#d95d7b",
+    dangerShade: "#F7DFE5",
+    success: "#41c6a7",
+    warning: "#FF8401",
+
+    primary: "#3139ce",
+    primaryShade: "#D6D7F5",
+    primaryShade2: "#4C54E8",
+    // kind of white
+    secondary: "#ffffff",
+    secondaryShade: "#e5e5e5",
+    secondaryShade2: "#FAFAFA",
+    // kind of black
+    tertiary: "#25253e",
+    tertiaryShade: "#9b9b9b",
+    tertiaryShade2: "#545462",
+  },
+  shadow: {
+    xl: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+  },
+};
